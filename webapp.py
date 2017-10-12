d554ca58c6a6e5b41d119af840eb216531603313
hoganavery49 <hogan.avery49@gmail.com>
2017-10-12 09:37:02 -0700

    fixed random python crap

--- webapp.py
+++ webapp.py
@@ -6,11 +6,23 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 def render_main():
     return render_template('home.html')
 
+@app.route("/pounds-to-kilos")
+def render_pounds_conversion():
+    return render_template('lbs-to-kgs.html')
+
+@app.route("/grams-to-moles")
+def render_grams_conversion():
+    return render_template('grams-to-moles.html')
+
+@app.route("/meters-to-femtometers")
+def render_meters_conversion():
+    return render_template('meters-to-femtometers.html')
+
 @app.route("/response")
 def render_response():
     if 'pounds' in request.args:
         reply = float(request.args['pounds'])*0.45359237
-    else if 'grams' in request.args:
+    elif 'grams' in request.args:
         reply = float(request.args['grams'])/float(request.args['molar-mass'])
     else:
         reply = float(request.args['meters'])*0.0000000000000015
